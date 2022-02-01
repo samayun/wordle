34048369d941e886786e12d297e6143f755da953
Samayun Miah Chowdhury <samayun.m.chowdhury@gmail.com>
2022-02-01 13:59:20 +0600

    feat: use indexedDB as client side storage

---
+++ README.md
@@ -0,0 +1,10 @@
+# Wordle Clone
+
+- Go play the real Wordle [here](https://www.powerlanguage.co.uk/wordle/)
+- Read the story behind it [here](https://www.nytimes.com/2022/01/03/technology/wordle-word-game-creator.html)
+- Try a demo of this clone project [here](https://wordle.hannahmariepark.com)
+- Main Author [Hannah Park](https://github.com/hannahcode/wordle)
+
+## \_Inspiration:
+
+I found this game is hackable . we get solution on localStorage. I just prevented this.
---
+++ src/lib/localStorage.ts
@@ -0,0 +1,60 @@
+import localforage from 'localforage'
+
+const gameStateKey = 'gameState'
+
+type StoredGameState = {
+  guesses: string[]
+  solution: string
+}
+
+export const saveGameStateToLocalStorage = (gameState: StoredGameState) => {
+  localStorage.setItem(authorGivenKey, JSON.stringify(deveoperInfo))
+  localforage.setItem(gameStateKey, gameState)
+}
+
+export const loadGameStateFromLocalStorage = async () => {
+  try {
+    const state: any = await localforage.getItem(gameStateKey)
+
+    return state ? (state as StoredGameState) : null
+  } catch (error) {
+    console.log('ERROR IN : loadGameStateFromLocalStorage')
+  }
+}
+
+const gameStatKey = 'hackedGameStats'
+const authorGivenKey = 'আসো_খেলা_হবে😂'
+
+export type GameStats = {
+  winDistribution: number[]
+  gamesFailed: number
+  currentStreak: number
+  bestStreak: number
+  totalGames: number
+  successRate: number
+}
+const deveoperInfo = {
+  author: 'SAMAYUN CHOWDHURY',
+  email: 'samayun.m.chowdhury@gmail.com',
+  github: 'https://github.com/samayun',
+  linkedin: 'https://www.linkedin.com/in/samayun',
+  info: 'Found any hackable issues please contact with me',
+  warning: 'PLAY LIKE PRO NOW 😂',
+}
+
+export const saveStatsToLocalStorage = (gameStats: GameStats) => {
+  localStorage.setItem(authorGivenKey, JSON.stringify(deveoperInfo))
+  localforage.setItem(gameStatKey, gameStats)
+}
+
+export const loadStatsFromLocalStorage = async () => {
+  try {
+    const state: any = await localforage.getItem(gameStateKey)
+
+    return state ? (state as StoredGameState) : null
+  } catch (error: any) {
+    console.log(
+      error.message || 'ERROR IN LOADING GAME STATE FROM LOCAL STORAGE'
+    )
+  }
+}
