ba1bc7d44676241fbf80018e3fe3035e91c30dfd
Samayun Miah Chowdhury <samayun.m.chowdhury@gmail.com>
2022-02-02 13:56:23 +0600

    chore(title):

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,10 +1,10 @@
-export const WORDLE_TITLE = 'Not Wordle'
+export const WORDLE_TITLE = 'Xoss Wordle'
 
 export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const ABOUT_GAME_MESSAGE = 'About this game'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WORD_NOT_FOUND_MESSAGE = 'Not in wordlist'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
   `The word was ${solution}`
 export const ENTER_TEXT = 'Enter'
